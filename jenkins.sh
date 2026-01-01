bbd5a14874faad8d2de06fb220286674f38dc456
Kishor Bhairat <155791075+kishor-95@users.noreply.github.com>
2026-01-01 10:11:22 +0530

    Refactor jenkins.sh for Debian package management
    Updated the Jenkins installation script for Debian-based systems, replacing YUM commands with APT commands and modifying the key import process.

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 ## First we add repos
-echo "Adding Jenkins repo..."
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-
-## Importing the key for Jenkins
 echo "Importing key..."
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  
 if [[ $? == 0 ]]; then
   echo "Key Imported"
 else
@@ -16,11 +13,11 @@ fi
 
 ## Update the new package list including Jenkins
 echo "Updating the packages..."
-sudo yum upgrade -y
+sudo apt upate -y && sudo apt upgrade -y
 
 ## Add required dependencies for the Jenkins package
 echo "Installing dependencies..."
-sudo yum install -y fontconfig java-21-openjdk
+sudo apt install fontconfig openjdk-21-jre -y
 if [[ $? == 0 ]]; then 
   echo "Dependencies installed successfully. Installing Jenkins..."
 else
@@ -29,16 +26,18 @@ else
 fi
 
 ## Installing Jenkins
-sudo yum install -y jenkins
+sudo apt install jenkins -y
 
 ## Start and enable the Jenkins service
 sudo systemctl daemon-reload
 sudo systemctl enable --now jenkins
 
 ## Adding Jenkins port in firewall
-echo "Opening port 8080 for Jenkins..."
-sudo firewall-cmd --add-port=8080/tcp --permanent
-sudo firewall-cmd --reload
+##echo "Opening port 8080 for Jenkins..."
+##sudo firewall-cmd --add-port=8080/tcp --permanent
+##sudo firewall-cmd --reload
 
 echo "✅ Jenkins installation and setup completed successfully!"
 echo "Access Jenkins at: http://<your-server-ip>:8080"
+
+
